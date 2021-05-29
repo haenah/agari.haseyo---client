7bdf4d4398a41134feca59b26749bfe7912f7cc2
jaewon <paramecium6@gmail.com>
2021-05-30 01:33:48 +0900

    초기 개발환경 야무진 세팅

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+/** @type {import('webpack').Configuration} */
+const config = {
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
+/** @type {import('webpack-dev-server').Configuration} */
+const devServerConfig = {
+  contentBase: path.join(__dirname, 'dist'),
+  port: 8080,
+  historyApiFallback: true,
+};
+
+config.devServer = devServerConfig;
+
+module.exports = config;
