9c3dffc9d7cf5adb7aa72aa9d247e460931f0dbe
jaewon <paramecium6@gmail.com>
2021-05-30 14:55:05 +0900

    웹소켓 연결 테스트를 위한 움직임

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { init } from './init';
+import * as uuid from 'uuid';
 
 try {
   init();
+  const webSocket = new WebSocket(`ws://localhost:8080?id=${uuid.v4()}&username=haenah`);
+  webSocket.onmessage = console.log;
 } catch (error) {
   console.error(error);
 }
